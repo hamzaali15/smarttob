0cde867c4fbafa9087739d48b46c785b67da65ef
hamzaali15 <malikhamzaali48@gmail.com>
2021-10-17 07:30:23 +0000

    SE updated for additional cost

--- smarttob/hook_events/bom.py
+++ smarttob/hook_events/bom.py
@@ -8,23 +8,24 @@ def calculate_costing(self, method):
 	self.db_update()
 
 def add_expense_account(self, method):
-	if self.get('__islocal'):
-		if self.stock_entry_type == "Manufacture":
-			if self.from_bom == 1:
-				indirect_total_cost = frappe.db.get_value("Work Order", self.work_order, 'indirect_total_cost')
-				smart_settings = frappe.get_single('SMART Settings')
-				if not smart_settings.expense_account:
-					frappe.throw("Please Set Expense Account in SMART Settings")
-				indirect_total = 0
-				if indirect_total_cost:
-					indirect_total = indirect_total_cost * self.fg_completed_qty
-					self.append('additional_costs', {
-						'expense_account': smart_settings.expense_account,
-						'description': "Overheads Cost",
-						'amount': indirect_total
-						})
-					self.total_additional_costs += indirect_total
-	for d in self.items:
-		d.additional_cost = 0
-		if d.t_warehouse and d.is_finished_item and d.amount > 0:
-			d.additional_cost = self.total_additional_costs
+	self.set('additional_costs', [])
+	if self.stock_entry_type == "Manufacture":
+		if self.from_bom == 1:
+			indirect_total_cost = frappe.db.get_value("Work Order", self.work_order, 'indirect_total_cost')
+			smart_settings = frappe.get_single('SMART Settings')
+			if not smart_settings.expense_account:
+				frappe.throw("Please Set Expense Account in SMART Settings")
+			indirect_total = 0
+			if indirect_total_cost:
+				indirect_total = indirect_total_cost * self.fg_completed_qty
+				self.append('additional_costs', {
+					'expense_account': smart_settings.expense_account,
+					'description': "Overheads Cost",
+					'amount': indirect_total,
+					'base_amount': indirect_total
+					})
+				self.total_additional_costs += indirect_total
+		for d in self.items:
+			d.additional_cost = 0
+			if d.t_warehouse and d.is_finished_item and d.amount > 0:
+				d.additional_cost = self.total_additional_costs
